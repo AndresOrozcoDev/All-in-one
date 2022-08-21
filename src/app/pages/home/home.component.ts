417470c6ba4149609eb94850499c9ec3ef0bfbf5
Andres Orozco <andres.orozco97@hotmail.com>
2022-08-21 03:24:04 -0400

    create variables and structure styles

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EmailValidator, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  signInForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.pattern('a-zA-Z')]),
+    password: new FormControl('', [Validators.required, Validators.pattern('0-9')])
+  });
+
+  showPassword: boolean = false;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showHidePassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  get email() { 
+    return this.signInForm.get('email'); 
+  }
+
+  get password() { 
+    return this.signInForm.get('password'); 
   }
 
 }
